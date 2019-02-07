f3c8e10e68b7285d2db4de58e73e0f3d910d12eb
Tyler Hunt <tyhunt04@gmail.com>
2019-02-07 02:09:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,3 +10,30 @@ end
 
 board = [" "," "," "," "," "," "," "," "," ",]
 display_board(board)
+
+board = [" "," "," "," ","X"," "," "," "," ",]
+display_board(board)
+
+board = ["O"," "," "," "," "," "," "," "," ",]
+display_board(board)
+
+board = ["O"," "," "," ","X"," "," "," "," ",]
+display_board(board)
+
+board = ["X","X","X"," "," "," "," "," "," ",]
+display_board(board)
+
+board = [" "," "," "," "," "," ","O","O","O",]
+display_board(board)
+
+board = ["X"," "," "," ","X"," "," "," ","X",]
+display_board(board)
+
+board = [" "," ","O"," ","O"," ","O"," "," ",]
+display_board(board)
+
+board = [" "," "," "," "," "," "," "," "," ",]
+display_board(board)
+
+board = ["X","X","X","X","X","X","X","X","X"]
+display_board(board)
